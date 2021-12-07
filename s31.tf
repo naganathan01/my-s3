245dbd1f1d8728ab894dddc37903228372856a64
naganathan01 <93266258+naganathan01@users.noreply.github.com>
2021-12-07 04:55:25 -0500

    Add files via upload

---
+++ s31.tf
@@ -0,0 +1,29 @@
+provider "aws"{
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "nathan-bucket"
+  acl    = "private"
+  
+
+  tags = {
+    Name        = "My-bucket"
+    Environment = "Dev"
+  }
+}
+
+
+
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = "nathan-bucket"
+  key    = "a1.html"
+  source = "C:/Users/Naganathan/frdemo/a1.html"
+
+  
+  etag = filemd5("C:/Users/Naganathan/frdemo/a1.html")
+}
+
+
+
